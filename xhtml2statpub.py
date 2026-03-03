cd47f3a51a87c2722b4c69a4736450652cc12edd
s-lhj <110468814+s-lhj@users.noreply.github.com>
2026-03-03 16:24:29 +0100

    fix grade-bug

--- xhtml2statpub.py
+++ xhtml2statpub.py
@@ -6187,7 +6187,7 @@ def _row_headings(table, header_row=None):
                 out.append(txt)
     return out
 
-def _mk_ul(items, cls):
+def _mk_ul_items(items, cls):
     if not items:
         return None
     ul = soup.new_tag("ul")
@@ -7394,7 +7394,7 @@ def apply_requirements(args, logger, soup, folders, comic_text_rpc=None):
         args.grade = 10
     '''
 
-    if args.grade and args.grade.isdigit():
+    if args.grade:
         args.grade = int(args.grade)
     else:
         for keyword in soup('meta', attrs={'name':'dc:subject.keyword'}):
@@ -13126,8 +13126,8 @@ def apply_requirements(args, logger, soup, folders, comic_text_rpc=None):
             fc.string = cap_text
             fig.append(fc)
 
-        ul_cols = _mk_ul(cols, "col-headings")
-        ul_rows = _mk_ul(rows, "row-headings")
+        ul_cols = _mk_ul_items(cols, "col-headings")
+        ul_rows = _mk_ul_items(rows, "row-headings")
         if ul_cols: fig.append(ul_cols)
         if ul_rows: fig.append(ul_rows)
 
